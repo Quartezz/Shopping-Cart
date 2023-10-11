e6f23f50b924b5026973a9c7a9429c28630e0cd5
Quartezz <kamil.machel03@gmail.com>
2023-10-11 23:53:52 +0200

    added Trending section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Trending from "./components/Trending";
 
 const App = () => {
   return (
     <>
       <Header />
       <Home />
+      <Trending />
     </>
   );
 };
