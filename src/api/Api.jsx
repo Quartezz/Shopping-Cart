bbe30366c6dc120d5a2f92a81683da23518423da
Quartezz <kamil.machel03@gmail.com>
2023-10-12 00:49:12 +0200

    created api link

---
+++ src/api/Api.jsx
@@ -0,0 +1,3 @@
+const APIKEY = "c2d6a2375c1c40d09c3dbab74d2a132f";
+
+export default APIKEY;
---
+++ src/api/GameList.jsx
@@ -0,0 +1,3 @@
+import APIKEY from "./Api";
+
+const url = `https://api.rawg.io/api/games?key=${APIKEY}&date=2022-01-01,2023-10-10&ordering=-added`;
