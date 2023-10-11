fecdd0279e1ba60dd200db0b19e97e604d55e211
Quartezz <kamil.machel03@gmail.com>
2023-10-12 01:57:54 +0200

    created axios file

--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -1,3 +1,12 @@
+import axios from "axios";
 const APIKEY = "c2d6a2375c1c40d09c3dbab74d2a132f";
 
-export default APIKEY;
+const axiosCreate = axios.create({
+  baseURL: "https://api.rawg.io/api",
+});
+
+const getGenreList = axiosCreate.get(`/genres?key=${APIKEY}`);
+
+export default {
+  getGenreList,
+};
