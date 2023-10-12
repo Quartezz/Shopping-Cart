3dc2a8b8acecfe91a4d202f10f1f1ab533f82647
Quartezz <kamil.machel03@gmail.com>
2023-10-12 23:26:39 +0200

    styling changed and added section

--- src/sections/Trending.jsx
+++ src/sections/Trending.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import Api from "../api/Api";
 
 const Trending = () => {
-  const [allGameList, setAllGameList] = useState();
+  const [allGameList, setAllGameList] = useState([]);
   useEffect(() => {
     getAllGamesList();
   }, []);
@@ -10,6 +10,7 @@ const Trending = () => {
   const getAllGamesList = () =>
     Api.getGamesList.then((resp) => {
       setAllGameList(resp.data.results);
+      console.log(resp.data.results);
     });
 
   return (
@@ -18,16 +19,29 @@ const Trending = () => {
         <h2 className="text-3xl">Trending</h2>
         <i className="fa-solid fa-chevron-right text-2xl"></i>
       </a>
-      <div className="flex relative justify-center flex-wrap m-4">
-        <div className="m-4 width-card z-1 min-w-0 relative flex flex-col bg-slate-50">
-          siema
-        </div>
-        <div className="m-4 width-card z-1 min-w-0 relative flex flex-col bg-slate-50">
-          siema
-        </div>
-        <div className="m-4 width-card z-1 min-w-0 relative flex flex-col bg-slate-50">
-          siema
-        </div>
+      <div className="flex flex-wrap w-full relative m-4 justify-center items-center flex-1">
+        {allGameList.map(
+          (item, index) =>
+            index < 6 && (
+              <div className="m-4 z-1 min-w-0 relative sm:flex sm:flex-col hover-transition hover:hover-transform cursor-pointer flex items-center gap-2">
+                <img
+                  src={item.background_image}
+                  className="w-full h-full object-fill rounded-lg sm:h-[200px] sm:w-[400px]"
+                />
+                <div className="w-full flex justify-between">
+                  <span>{item.name}</span>
+                  <span className="flex gap-1 items-center">
+                    <i class="fa-regular fa-calendar"></i>
+                    {item.released}
+                  </span>
+                  <span className="flex gap-1 items-center">
+                    <i class="fa-solid fa-star"></i>
+                    {item.rating}
+                  </span>
+                </div>
+              </div>
+            )
+        )}
       </div>
     </div>
   );
