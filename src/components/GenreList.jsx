2a13061805d691d27aae1ae6edaf681296d0bbe0
Quartezz <kamil.machel03@gmail.com>
2023-10-12 21:48:58 +0200

    changed to flex container

--- src/components/GenreList.jsx
+++ src/components/GenreList.jsx
@@ -12,21 +12,27 @@ const GenreList = () => {
     });
   };
   return (
-    <div className="flex flex-col px-40 text-slate-100 bg-color w-full h-full">
+    <div className="sm:px-40 text-slate-100 bg-color w-full h-full px-10">
       <a href="/" className="flex items-end justify-start gap-2">
         <h2 className="text-3xl">Categories</h2>
         <i className="fa-solid fa-chevron-right text-2xl"></i>
       </a>
-      <div className="flex relative justify-center flex-wrap m-4">
-        {genreList.map((item) => (
-          <div className="m-4 width-card z-1 min-w-0 relative flex flex-col hover-transition hover:hover-transform cursor-pointer">
+      <div className="flex flex-wrap w-full relative m-4 justify-center items-center flex-1">
+        {genreList.map((item, index) => (
+          <div
+            className={`m-4 z-1 min-w-0 relative sm:flex sm:flex-col hover-transition hover:hover-transform cursor-pointer flex items-center gap-2${
+              index === genreList.length - 1 ? "center-last" : ""
+            }`}
+          >
             <img
               src={item.image_background}
               alt={item.name}
-              className="w-full h-full object-fill rounded-lg max-h-[200px]"
+              className="w-full h-full object-fill rounded-lg sm:h-[150px] sm:w-[250px] h-[75px] w-[100px]"
             />
-            <div className="text-center mt-2">
-              <div className="z-1 text-2xl font-bold">{item.name}</div>
+            <div className="sm:text-center mt-2 w-[90px] text-start">
+              <div className="z-1 sm:text-2xl font-bold text-xl">
+                {item.name}
+              </div>
             </div>
           </div>
         ))}
