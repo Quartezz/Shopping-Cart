62a73c53fbdd836d001a337427acf4df4adce815
Quartezz <kamil.machel03@gmail.com>
2023-10-12 23:25:51 +0200

    created constans

---
+++ src/constans/index.js
@@ -0,0 +1,32 @@
+import facebook from "../assets/icons/facebook.svg";
+import twitter from "../assets/icons/instagram.svg";
+import instagram from "../assets/icons/instagram.svg";
+
+export const footerLinks = [
+  {
+    title: "Help",
+    links: [
+      { name: "About us", link: "/" },
+      { name: "FAQs", link: "/" },
+      { name: "How it works", link: "/" },
+      { name: "Privacy policy", link: "/" },
+      { name: "Payment policy", link: "/" },
+    ],
+  },
+  {
+    title: "Get in touch",
+    links: [
+      {
+        name: "customer@gameharbor.com",
+        link: "mailto:customer@gameharbor.com",
+      },
+      { name: "+92554862354", link: "tel:+92554862354" },
+    ],
+  },
+];
+
+export const socialMedia = [
+  { src: facebook, alt: "facebook logo" },
+  { src: twitter, alt: "twitter logo" },
+  { src: instagram, alt: "instagram logo" },
+];
