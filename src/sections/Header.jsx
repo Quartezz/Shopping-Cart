afd714e4a717ad85331af1d4d87465749a2b4b55
Quartezz <kamil.machel03@gmail.com>
2023-10-13 14:38:16 +0200

    added link to cart and top of the page

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -4,9 +4,13 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <header className="flex items-center justify-between w-full py-0 px-[30px] h-20 fixed top-[0] z-10 mb-[-73px] text-slate-100 opacity-85 bg-color">
-      <a href="#home" alt="logo" className="flex gap-2">
-        <img src={logo} alt="logo" width={120} height={40} />
-      </a>
+      <div className="flex gap-2">
+        <Link to={"/"}>
+          <a alt="logo" className="">
+            <img src={logo} alt="logo" width={120} height={40} />
+          </a>
+        </Link>
+      </div>
       <div className="flex flex-col items-center justify-center gap-2">
         <div className="flex gap-4">
           <a
@@ -65,6 +69,9 @@ const Header = () => {
         </div>
       </div>
       <div className="flex gap-4">
+        <a href="#home">
+          <i className="fa-solid fa-circle-arrow-up text-2xl"></i>
+        </a>
         <Link to="/cart">
           <a href="" className="hover-transition hover:hover-transform">
             <i className="fa-solid fa-cart-shopping text-2xl"></i>
